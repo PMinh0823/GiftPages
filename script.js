a672fdf4ceee768e1fe407446de47820ebedff1a
PMinh0823 <gpqminh@gmail.com>
2026-07-22 07:57:51 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,41 @@
-// =========================
-// Get Elements
-// =========================
+const card = document.querySelector(".card");
 
-const introTitle = document.getElementById("intro-title");
-const introSubtitle = document.getElementById("intro-subtitle");
-const title = document.getElementById("title");
-const message = document.getElementById("message");
+window.addEventListener("load",()=>{
 
-// =========================
-// Fill Content
-// =========================
+    card.classList.add("show");
 
-introTitle.textContent = config.introTitle;
-introSubtitle.innerHTML = config.introSubtitle.replace(/\n/g, "<br>");
+});
 
-title.textContent = config.pageTitle;
-message.innerHTML = config.message.replace(/\n/g, "<br>");
+function createHeart(){
+
+    const heart=document.createElement("div");
+
+    heart.className="heart";
+
+    heart.innerHTML="❤";
+
+    heart.style.left=Math.random()*100+"vw";
+
+    heart.style.fontSize=
+
+        12+Math.random()*20+"px";
+
+    heart.style.animationDuration=
+
+        5+Math.random()*5+"s";
+
+    document.body.appendChild(heart);
+
+    setTimeout(()=>{
+
+        heart.remove();
+
+    },10000);
+
+}
+
+setInterval(createHeart,600);
+
+document.querySelector("#year").textContent=
+
+new Date().getFullYear();
